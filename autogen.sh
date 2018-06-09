3a6bc2706dc5d40468251c660b4d18b81945b845
storynara <tory.jeong@gmail.com>
2018-06-10 00:53:59 +0900

    modify autogen.sh
    Signed-off-by: storynara <tory.jeong@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@ set -x
 
 test -d autom4te.cache && rm -rf autom4te.cache
 libtoolize || exit 1
-aclocal -I config || exit 1
+aclocal || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake --add-missing --copy || exit 1
